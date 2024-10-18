0648d02c2d10f91876ec1db1eaf1e40761467803
summer-mountain <st20072@t.kagawa-nct.ac.jp>
2024-10-18 15:03:21 +0900

    betatest

---
+++ asynchMulchiArray/README.md
@@ -0,0 +1,145 @@
+# マイクロホンアレイ間の非同期性を模擬可能なインパルス応答データセット
+
+## 概要
+このインパルス応答データセットは，全てのマイクロホンが完全同期された状態で収録されたものです．マイクロホンアレイ間の同期はできないという想定の下，シミュレーションにより録音開始時刻及びサンプリング周波数のズレを設定することで，マイクロホンアレイ間の非同期条件を再現することを目的としています．このデータセットは音楽練習室及びオープンラウンジの2箇所で，かつ様々な状況の下，収録しました．また，シミュレーションを実施するためのスクリプトも同梱しています．
+
+## フォルダ構成について
+~~~
+impulse/
+├── impulse_dataset/  # 入力となるWAVファイルを格納するフォルダ
+│   └── (120ファイル)
+├── processWavFiles
+├── image/
+|    └── (19ファイル)
+└── readme.md
+~~~
+
+## データセットについて
+- **格納場所**: impulse_detaset/roomType_recCond_soundSource_ir_micCh.wav
+  - **roomType**: 収録場所(オープンラウンジ：openLounge, 音楽練習室：musicRoom)
+  - **recCond**: 収録環境(2A, 2B, 2C, 3A, 3B)
+  - **soundSource**: 音源(int1, int2, int3, target)
+  - **micCh**: マイクロホンアレイのチャンネル(1~12)
+
+## スクリプトについて
+MATLABのコマンドウィンドウ上で下記の関数を実行することで，所望する録音開始時刻及びサンプリング周波数のズレが適用されたWAVファイルを出力することができます．
+- **構文**: `processWavFiles(roomType, recCond, fs, recTimeDelay, recFsDeviation)`
+
+引数|説明|入力形式
+-|-|-
+`roomType`|収録場所<br>(オープンラウンジ：openLounge, 音楽練習室：musicRoom)|'openLounge'/'musicRoom'<br>詳細は[マイクロホンアレイとスピーカーの配置](#マイクロホンアレイとスピーカーの配置)を参照
+`recCond`|収録環境(2A, 2B, 2C, 3A, 3B)|'2A'/'2B'/'2C'/'3A'/'3B'<br>詳細は[収録環境](#収録環境)を参照
+`fs`|所望するサンプリング周波数<br>(単位は[Hz])|区間(0, 96000]内の整数値
+`recTimeDelay`|所望する録音開始時刻の遅延<br>(単位は[s])|マイクロホンアレイごとの<br>遅延時間を含む行ベクトル
+`recFsDeviation`|所望するサンプリング周波数の<br>基準値(fs)からのずれ(単位は[ppm])|マイクロホンアレイごとの<br>ずれを含む行ベクトル
+ 
+### 例（マイクロホンアレイが2個の場合）
+`processWavFiles("openLounge", "2A", 48000, [0, 2], [0, 10])`
+- 収録場所は"openLounge"
+- 収録環境は"2A"
+- 所望するサンプリング周波数は48000 Hz
+- 2つ目のマイクロホンアレイの録音開始時刻を2 s遅らせる
+- 2つ目のマイクロホンアレイのサンプリング周波数を2 ppmずらす
+
+### 例（マイクロホンアレイが3個の場合）
+`processWavFiles("musicRoom", "3B", 96000, [1, 2, 3], [2, 4, 6])`
+- 収録場所は"musicRoom"
+- 収録環境は"3B"
+- 所望するサンプリング周波数は96000 Hz
+- 1つ目のマイクロホンアレイの録音開始時刻を1 s，2つ目を2 s，3つ目を3 s遅らせる
+- 1つ目のマイクロホンアレイのサンプリング周波数をを2 ppm，2つ目を4 ppm，3つ目を6 ppmずらす
+
+## 出力ファイルについて
+関数processWavFilesを実行することで，impulseフォルダ内に新たにoutputフォルダが生成され，その中に出力ファイルが生成されます．出力ファイル名は関数に指定したサンプリング周波数，録音開始時刻の遅延，及びサンプリング周波数のずれが表示されるようになっています．
+
+### 例
+- `processWavFiles("openLounge", "2A", 48000, [0, 2], [0, 10])`を実行した場合
+  - **出力ファイル名**: soundSrc_fs48000_td0.000-2.000_fd0.000-10.000_micCh.wav
+    - **soundSrc**: 音源（int1, int2, targetの3種類）
+    - **td**: 指定した録音開始時刻の遅延
+    - **fd**: 指定したサンプリング周波数のずれ
+    - **micCh**: マイクロホンのチャンネル（1-4, 9-12の8チャンネル）
+
+- processWavFiles("musicRoom", "3B", 96000, [1, 2, 3], [2, 4, 6])を実行した場合
+  - **出力ファイル名**: soundSrc_fs96000_td1.000-2.000-3.000_fd2.000-4.000-6.000_micCh.wav
+    - **soundSrc**: 音源（int1, int2, int3, targetの4種類）
+    - **td**: 指定した録音開始時刻の遅延
+    - **fd**: 指定したサンプリング周波数のずれ
+    - **micCh**: マイクロホンのチャンネル（1-12の12チャンネル）
+
+## 測定環境
+
+### 使用機材
+
+- **マイクロホン**: JTS CX-500（コンデンサマイク）
+  - 4つのマイクロホンを等間隔で直線状に配置して1つのマイクロホンアレイを構成
+![alt text](./image/image.png)
+- **スピーカー**: AURATONE 5C Super Sound Cube
+![alt text](./image/image-1.png)
+- **パワーアンプ**: AURATONE A2-30（5C Super Sound Cube専用）
+![alt text](./image/image-2.png) ![alt text](./image/image-3.png)
+- **オーディオインターフェース**: YAMAHA DM3（96kHz/32ビットのサンプリング周波数で最大8チャンネルの同期再生と16チャンネルの同期録音が可能）
+![alt text](./image/image-4.png)
+- **AD/DAコンバーター**: Tio1608-D2（全チャンネルの同期AD/DA変換を実現）
+- **ソフトウェア**: MATLABのImpulse Response Measurerツール
+  - Sample Rate [Hz] : 96000
+  - Samples per Frame : 1024
+  - Method : Swept sine
+    - Wait before first run : 0
+    - Pause between runs : 0
+    - Number of warm-up runs : 0
+    - Sweep start frequency : 10 Hz
+    - Sweep stop frequency : 22000 Hz
+    - Sweep duration : 6 s
+  - Number pf Runs : 10
+  - Duration of Runs [s] : 10
+  - Excitation Level [dBFS] : -6
+
+### システム構成図
+![alt text](./image/image-5.png)
+
+## マイクロホンアレイとスピーカーの配置
+
+### 音楽練習室
+
+- **室温**: 16°C
+- **湿度**: 45%
+- **部屋の大きさ**: 8.5 m × 6.7 m ×  m
+![alt text](./image/image-6.png) ![alt text](./image/image-7.png)
+- **マイクロホンアレイの高さ**: 床から1.2 m
+- **スピーカーコーンの高さ**: 床から1.2 m
+![alt text](./image/image-11.png)
+- **マイクロホンアレイの間隔**: 1 cm
+![alt text](./image/image-26.png)
+
+### オープンラウンジ
+
+#### 実験環境
+- **室温**: 19°C
+- **湿度**: 35%
+- **部屋の大きさ**: 8 m × 7 m × 2.2 m
+![alt text](./image/image-14.png) ![alt text](./image/image-15.png)
+- **マイクロホンアレイの高さ**: 床から1.2 m
+- **スピーカーコーンの高さ**: 床から1.2 m
+![alt text](./image/image-16.png)
+- **マイクロホンアレイの間隔**: 1 cm
+![alt text](./image/image-27.png)
+
+## 収録環境
+以下の5つの状況をもとに、複数のマイクロホンアレイ及び音源を配置しました．
+
+- **マイクロホンアレイが2個のケース**
+
+![alt text](./image/image-18.png)
+
+![alt text](./image/image-19.png)
+
+![alt text](./image/image-20.png)
+
+- **マイクロホンアレイが3個のケース**
+
+![alt text](./image/image-21.png)
+
+![alt text](./image/image-22.png)
+
+---
---
+++ asynchMulchiArray/processWavFiles.m
@@ -0,0 +1,246 @@
+% Function to process WAV files according to the user's desired roomType and recCond
+function processWavFiles(roomType, recCond, fs, recTimeDelay, recFsDeviation)
+
+    % Set directory and sound source names
+    dirName = "./impulse_dataset/";
+    srcName = ["target", "int1", "int2", "int3"];
+
+    % Number of sound sources
+    nSrc = str2double(extractBefore(recCond, 2)) + 1; % extract
+
+    % List of recording channels
+    if     nSrc == 3 % case of either 2A. 2B, or 2C
+        listCh = [1:4, 9:12];
+    elseif nSrc == 4 % case of either 3A, or 3B
+        listCh = 1:12;
+    else
+        error("The input argument 'recCond' is incorrect.\n");
+    end
+
+    % Create 2D string array for input filepath
+    inFilePath = strings(nSrc, length(listCh));
+
+    % Create file paths
+    for iSrc = 1:nSrc
+        for iCh = 1:length(listCh)
+            inFilePath(iSrc, iCh) = dirName + roomType + "_" + recCond + "_" + srcName(iSrc) + "_ir_" + listCh(iCh) + ".wav";
+        end
+    end
+
+    % Create 2D array for output filepath
+    outFilePath = strings(nSrc, length(listCh));
+
+    % Generate output file paths and call resampleCorrectWav for each sound source
+    for iSrc = 1:nSrc
+        % for iCh = 1:length(listCh)
+        outFilePath(iSrc, :) = genOutFilePath(recCond, srcName(iSrc), fs, recTimeDelay, recFsDeviation);
+        % end
+
+        inCurrentFiles = inFilePath(iSrc, :);
+        outCurrentFiles = outFilePath(iSrc, :);
+        resampleCorrectWav(inCurrentFiles, outCurrentFiles, fs, recTimeDelay, recFsDeviation); 
+    end
+end
+
+% Function to apply desired fs, recTimeDelay, and recFsDeviation
+function resampleCorrectWav(inputFiles, outputFiles, inputFs, recTimeDelay, recFsDeviation)
+
+    % Set default sample rate
+    defaultFs = 96000; % [Hz]
+
+    % Set sample rate
+    if inputFs > 0 && inputFs <= defaultFs
+        fs = inputFs;
+        disp(['Specified sample rate: fs = ', num2str(fs), ' Hz']);
+    else
+        error('Invalid sample rate. Fs must be greater than 0 Hz and less than 96 kHz.');
+    end
+
+    % Number of samples
+    audioData = audioread(inputFiles{1});
+    numSamples = length(audioData);
+
+    % Number of input files
+    numFiles = length(inputFiles);
+    
+    % Initialize 2D array to store resampled data
+    resampledData = zeros(numFiles, numSamples);
+
+    % Process each input file
+    for i = 1:numFiles
+        inputFile = inputFiles{i};
+
+        % Loading WAV files
+        [currentAudioData, originalFs] = audioread(inputFile);
+    
+        % Check whether resample is required
+        if fs ~= originalFs
+            p = fs;         % New sample rate
+            q = originalFs; % Original sample rate
+            n = 100;        % Length of the FIR filter
+
+            % Resample with specified filter length
+            resampledAudioData = resample(currentAudioData, p, q, n);
+            currentNumSamples = length(resampledAudioData);
+            resampledData(i, 1:currentNumSamples) = resampledAudioData;
+
+            disp(['Audio data was resampled from ', num2str(originalFs), ' Hz to ', num2str(fs), ' Hz.']);
+        else
+            resampledData(i, :) = audioData;
+            disp('No resampling was required.');
+        end
+    end
+
+    % Apply recTimeDelay
+    timeProcessedData = applyRecTimeDelay(resampledData, fs, recTimeDelay);
+
+    % Apply recFsDeviation
+    applyRecFsdeviation(timeProcessedData, fs, recFsDeviation, outputFiles);
+
+end
+
+% Function to generate delay of recording time
+function audioDataDelayed = applyRecTimeDelay(audioData, fs, recTimeDelay)
+
+    % Transpose audioData
+    tAudioData = audioData.';
+
+    % Convert recTimeDelay units from [s] to [samples]
+    delaySamples = round(recTimeDelay * fs); % integer value
+
+    % Identification of the number of channels
+    numChannels = size(audioData, 1);
+
+    % Identification of the number of mic array
+    if numChannels == 8      % 2 mic arrays
+        groups = [1:4; 5:8];
+    elseif numChannels == 12 % 3 mic arrays
+        groups = [1:4; 5:8; 9:12];
+    else
+        error('The number of unsupported channels.');
+    end
+
+    % Check the number of mic arrays and elements in recTimeDelay
+    if length(recTimeDelay) ~= size(groups, 1)
+        error('The number of mic arrays dose not match the number of elements in recTimeDelay.');
+    end
+
+    % Calcurate max delay samples
+    maxDelay = max(recTimeDelay);
+    maxDelaySamples = round(maxDelay * fs);
+
+    % Apply delay of recording time
+    numSamples = size(audioData, 2);
+    audioDataDelayed = zeros(numSamples + maxDelaySamples, numChannels); % Initialization
+    for g = 1:size(groups, 1)
+        groupChannels = groups(g, :);
+        delay = delaySamples(g);
+
+        if delay > 0
+            % Fill in 0 for the displacement part
+            delayedData = [zeros(delay, length(groupChannels)); tAudioData(:, groupChannels)];
+            
+            % Store the delayed data in the appropriate section
+            audioDataDelayed(1:length(delayedData), groupChannels) = delayedData;
+        else
+            audioDataDelayed(1:length(tAudioData), groupChannels) = tAudioData(:, groupChannels);
+        end
+    end
+end
+
+% Function to apply sample rate deviation
+function applyRecFsdeviation(audioData, fs, recFsDeviation, inputFiles)
+
+    % comvert ppm value to sample rate
+    newFs = fs * (1 + recFsDeviation * 10^-6);
+
+    % Identification of the number of channels
+    numChannels = size(audioData, 2);
+
+    % Identification of the number of mic array
+    if numChannels == 8      % 2 mic arrays
+        groups = [1:4; 5:8];
+    elseif numChannels == 12 % 3 mic arrays
+        groups = [1:4; 5:8; 9:12];
+    else
+        error('The number of unsupported channels.');
+    end
+
+    % Check the number of mic arrays and elements in recFsDeviaiton
+    if length(recFsDeviation) ~= size(groups, 1)
+        error('The number of mic arrays dose not match the number of elements in recFsDeviation.');
+    end
+
+    % Calcurate max samples after resampling
+    maxPpm = max(recFsDeviation);
+    maxNewFs = fs * (1 + maxPpm * 10^-6);
+    maxNumSamples = ceil(size(audioData, 1) * (maxNewFs / fs));
+
+    % Initialize with extended size
+    audioDataAdjusted = zeros(maxNumSamples, numChannels);
+
+    % Apply sample rate deviation
+    for i = 1:size(groups, 1)
+        groupChannels = groups(i, :);
+
+        disp(['newFs = ', num2str(newFs(i))]);
+
+        % Create original time vector
+        tOriginal = (0:length(audioData(:, groupChannels)) - 1) / fs;
+
+        % Create a time vector based on the new sample rate
+        tNew = 0:1/newFs(i):(length(audioData(:, groupChannels)) - 1) / fs;
+
+        % Create griddedInterpolant object
+        F = griddedInterpolant(tOriginal, audioData(:, groupChannels), 'spline');
+
+        % Perform resampling based on new time vector
+        resampledData = F(tNew);
+
+        % Apply resampled data
+        audioDataAdjusted(1:length(resampledData), groupChannels) = resampledData;
+    end
+
+    % Output resampled data as a WAV file
+    for j = 1:size(groups, 1)
+        for k = 1:length(inputFiles)
+            audiowrite(inputFiles(k), audioDataAdjusted(:, k), fs);
+        end
+    end
+end
+
+% Function to generate output path
+function outputFiles = genOutFilePath(recCond, soundSrc, fs, recTimeDelay, recFsDeviation)
+
+    % Create output derectory
+    outputDir = "./output/";
+    if ~exist(outputDir, 'dir')
+        mkdir(outputDir);
+    end
+
+    % Number of sound sources
+    nSrc = str2double(extractBefore(recCond, 2)) + 1; % extract
+
+    % List of recording channels
+    if     nSrc == 3 % case of either 2A. 2B, or 2C
+        listCh = [1:4, 9:12];
+    elseif nSrc == 4 % case of either 3A, or 3B
+        listCh = 1:12;
+    else
+        error("The input argument 'recCond' is incorrect.\n");
+    end
+
+    % Create array for output file path
+    outputFiles = strings(1, length(listCh));
+
+    % Generate output file path
+    if nSrc == 3
+        for iCh = 1:length(listCh)
+            outputFiles(iCh) = outputDir + soundSrc + "_fs" + fs + "_td" + sprintf("%5.3f", recTimeDelay(1)) + "-" + sprintf("%5.3f", recTimeDelay(2)) + "_fd" + sprintf("%5.3f", recFsDeviation(1)) + "-" + sprintf("%5.3f", recFsDeviation(2)) + "_" + listCh(iCh) + ".wav";
+        end
+    elseif nSrc == 4
+        for iCh = 1:length(listCh)
+            outputFiles(iCh) = outputDir + soundSrc + "_fs" + fs + "_td" + sprintf("%5.3f", recTimeDelay(1)) + "-" + sprintf("%5.3f", recTimeDelay(2)) + "-" + sprintf("%5.3f", recTimeDelay(3)) + "_fd" + sprintf("%5.3f", recFsDeviation(1)) + "-" + sprintf("%5.3f", recFsDeviation(2)) + "-" + sprintf("%5.3f", recFsDeviation(3)) + "_" + listCh(iCh) + ".wav";
+        end
+    end
+end
